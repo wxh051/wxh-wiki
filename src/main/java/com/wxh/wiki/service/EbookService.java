ba0084eff788abed10ce244ea67f8e41cca6b340
wxh <13546675295@163.com>
2022-06-02 22:29:21 +0800

    3-8 制作CopyUtil

--- src/main/java/com/wxh/wiki/service/EbookService.java
+++ src/main/java/com/wxh/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.wxh.wiki.domain.EbookExample;
 import com.wxh.wiki.mapper.EbookMapper;
 import com.wxh.wiki.req.EbookReq;
 import com.wxh.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.wxh.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,12 +29,18 @@ public class EbookService {
         //持久层返回List<Ebook>需要转换成List<EbookResp>再返回Controller
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp>respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp=new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        // List<EbookResp>respList = new ArrayList<>();
+        // for (Ebook ebook : ebookList) {
+        //    // EbookResp ebookResp=new EbookResp();
+        //    // BeanUtils.copyProperties(ebook,ebookResp);
+        //    //对象复制
+        //     EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //     respList.add(ebookResp);
+        // }
+
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
