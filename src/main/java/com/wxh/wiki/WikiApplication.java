7bc6f8f5002f22a8c9cd5dd7e1719f3d015c869d
wxh <13546675295@163.com>
2022-06-02 11:17:20 +0800

    2-5 修改启动文案

--- src/main/java/com/wxh/wiki/WikiApplication.java
+++ src/main/java/com/wxh/wiki/WikiApplication.java
@@ -1,7 +1,11 @@
 package com.wxh.wiki;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * @author wxh
@@ -9,8 +13,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
+
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
