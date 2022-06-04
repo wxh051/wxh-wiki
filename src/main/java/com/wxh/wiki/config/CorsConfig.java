92c47eb5f1f8ac02d79c429289e525d8491a0e78
wxh <13546675295@163.com>
2022-06-04 16:47:12 +0800

    5-3 使用ref和reactive实现数据绑定

--- src/main/java/com/wxh/wiki/config/CorsConfig.java
+++ src/main/java/com/wxh/wiki/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * @author wxh
  * @date 2022-06-04 15:20
+ * 跨域问题（后端不信任前端）解决
  */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -20,7 +21,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")
                 .allowedHeaders(CorsConfiguration.ALL)
                 .allowedMethods(CorsConfiguration.ALL)
-                //凭证，允许前端带上它的凭证信息，比如token
+                //凭证，允许前端带上它的凭证信息，比如token。因为每次前端发过来的请求，会生成一个新的会话
                 .allowCredentials(true)
                 .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
     }
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,24 +48,49 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <pre>
+{{ ebooks }}
+{{ ebooks2 }}
+      </pre>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-//  Vue3新增的初始化方法。这个组件加载完后会去加载这个方法
+  //Vue3新增的初始化方法。这个组件加载完后会去加载这个方法
+  //setup就放一些参数定义，方法定义
   setup() {
     console.log("setup");
-    //跨域问题（后端不信任前端）解决
-    axios.get("http://localhost:8888/ebook/list?name=Spring").then((response) => {
-      console.log(response);
+    //ref():响应式数据，在js里动态修改里面的值，实时反馈到页面上
+    const ebooks = ref();
+    //定义一个变量，实际上返回的是里面的属性，将这个属性变成一个响应式变量
+    const ebooks1 = reactive({books: []});
+
+
+    //一般要初始化的一些逻辑，建议都写到生命周期函数里。
+    // 如果写在setup方法里，有时候setup执行的时候界面还没渲染好，这时候如果去操作界面元素会报错
+    onMounted(() => {
+      console.log("onMounted");
+      axios.get("http://localhost:8888/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content
+        ebooks1.books = data.content;
+        console.log(response);
+      });
     })
+
+    //HTML代码拿到响应式变量，需要在setup最后return
+    return {
+      ebooks,
+      //返回，只需要返回books这段就可以；通过toref变成响应式变量
+      //另有一个torefs，可以将所有属性变成响应式变量
+      ebooks2: toRef(ebooks1, "books")
+    }
   }
 });
 </script>
