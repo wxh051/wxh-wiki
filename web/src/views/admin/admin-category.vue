3a628db106b5a34e1d0da12ebf245b3630412df5
wxh <13546675295@163.com>
2022-06-07 19:13:44 +0800

    7-5 父分类改为下拉选择

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -59,7 +59,16 @@
         <a-input v-model:value="category.name"/>
       </a-form-item>
       <a-form-item label="父分类">
-        <a-input v-model:value="category.parent"/>
+        <a-select
+            ref="select"
+            v-model:value="category.parent"
+        >
+          <a-select-option value="0">无</a-select-option>
+          <!--          c就是代表一行一行的分类，下面的ID，name和表结构是对应的;HTML使用变量要用{{}}-->
+          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id===c.id">
+            {{ c.name }}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="category.sort"/>
