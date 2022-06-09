75ce24f8e11ae9f9771f61e5826ae75a4d57bd44
wxh <13546675295@163.com>
2022-06-09 21:08:43 +0800

    9-9 增加redis测试接口，通过日志里获取的token值。确实得到了存入redis的信息

--- src/main/java/com/wxh/wiki/controller/TestController.java
+++ src/main/java/com/wxh/wiki/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.wxh.wiki.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wxh
@@ -15,8 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
     @Value("${test.hello}")
     private String testhello;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     /**
      * GET PUT POST DELETE
@@ -30,5 +39,19 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name) {
         return "hello world! Post," + name;
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
