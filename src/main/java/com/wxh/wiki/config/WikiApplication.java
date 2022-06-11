cbbc5784409ff5397a4b73ceee4a39ec013c9d7a
wxh <13546675295@163.com>
2022-06-11 14:05:16 +0800

    10-5 定时任务示例

--- src/main/java/com/wxh/wiki/config/WikiApplication.java
+++ src/main/java/com/wxh/wiki/config/WikiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author wxh
@@ -18,6 +19,7 @@ import org.springframework.core.env.Environment;
 @ComponentScan("com.wxh")
 @MapperScan("com.wxh.wiki.mapper")
 @SpringBootApplication
+@EnableScheduling
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
