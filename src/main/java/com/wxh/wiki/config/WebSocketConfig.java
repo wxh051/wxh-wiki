239c9fdf0e9442c0ce9f2b1a9eb7dce81c4c8bcb
wxh <13546675295@163.com>
2022-06-11 16:20:45 +0800

    10-8 集成WebSocket

---
+++ src/main/java/com/wxh/wiki/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.wxh.wiki.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author wxh
+ * @date 2022-06-11 15:50
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
