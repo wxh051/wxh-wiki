341235ff82abafc6ee5406fdaad2d48c6a74d4ba
wxh <13546675295@163.com>
2022-06-11 17:20:23 +0800

    10-10 加入事务注解

--- src/main/java/com/wxh/wiki/service/DocService.java
+++ src/main/java/com/wxh/wiki/service/DocService.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import java.util.List;
@@ -100,6 +101,7 @@ public class DocService {
      *
      * @param req
      */
+    @Transactional
     public void save(DocSaveReq req) {
         //doc.id==content.id
         Doc doc = CopyUtil.copy(req, Doc.class);
