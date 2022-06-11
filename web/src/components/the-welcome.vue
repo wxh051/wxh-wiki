68118022669c7858d259a1b8a271f18c697790cb
wxh <13546675295@163.com>
2022-06-11 23:54:19 +0800

    11-6 前端增加数值统计组件，显示统计数值

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -1,16 +1,139 @@
 <template>
-  <h1>欢迎使用xh知识库</h1>
+  <div>
+    <a-row>
+      <a-col :span="24">
+        <a-card>
+          <a-row>
+            <a-col :span="8">
+              <a-statistic title="总阅读量" :value="statistic.viewCount">
+                <template #suffix>
+                  <UserOutlined/>
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="8">
+              <a-statistic title="总点赞量" :value="statistic.voteCount">
+                <template #suffix>
+                  <like-outlined/>
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="8">
+              <a-statistic title="点赞率" :value="statistic.voteCount / statistic.viewCount * 100"
+                           :precision="2"
+                           suffix="%"
+                           :value-style="{ color: '#cf1322' }">
+                <template #suffix>
+                  <like-outlined/>
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+    </a-row>
+    <br>
+    <a-row :gutter="16">
+      <a-col :span="12">
+        <a-card>
+          <a-row>
+            <a-col :span="12">
+              <a-statistic title="今日阅读" :value="statistic.todayViewCount" style="margin-right: 50px">
+                <template #suffix>
+                  <UserOutlined/>
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="12">
+              <a-statistic title="今日点赞" :value="statistic.todayVoteCount">
+                <template #suffix>
+                  <like-outlined/>
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+      <a-col :span="12">
+        <a-card>
+          <a-row>
+            <a-col :span="12">
+              <a-statistic
+                  title="预计今日阅读"
+                  :value="statistic.todayViewIncrease"
+                  :value-style="{ color: '#0000ff' }"
+              >
+                <template #suffix>
+                  <UserOutlined/>
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="12">
+              <a-statistic
+                  title="预计今日阅读增长"
+                  :value="statistic.todayViewIncreaseRateAbs"
+                  :precision="2"
+                  suffix="%"
+                  class="demo-class"
+                  :value-style="statistic.todayViewIncreaseRate < 0 ? { color: '#3f8600' } : { color: '#cf1322' }"
+              >
+                <template #prefix>
+                  <arrow-down-outlined v-if="statistic.todayViewIncreaseRate < 0"/>
+                  <arrow-up-outlined v-if="statistic.todayViewIncreaseRate >= 0"/>
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref, onMounted} from 'vue'
+import axios from 'axios';
 
 export default defineComponent({
   name: 'the-welcome',
   setup() {
-    
-    return {
+    const statistic = ref();
+    statistic.value = {};
+    const getStatistic = () => {
+      // 接口返回数组：【昨天，今天】
+      // 今天是第一天，昨天是第0条（后端查询时加了一个排序）
+      axios.get('/ebook-snapshot/get-statistic').then((response) => {
+        const data = response.data;
+        if (data.success) {
+          const statisticResp = data.content;
+          statistic.value.viewCount = statisticResp[1].viewCount;
+          statistic.value.voteCount = statisticResp[1].voteCount;
+          statistic.value.todayViewCount = statisticResp[1].viewIncrease;
+          statistic.value.todayVoteCount = statisticResp[1].voteIncrease;
 
+          // 按分钟计算当前时间点，占一天的百分比
+          const now = new Date();
+          //一天过去了多少分钟
+          const nowRate = (now.getHours() * 60 + now.getMinutes()) / (60 * 24);
+          // console.log(nowRate)
+          //今天预计=今天到目前的阅读量/（当前时间点占一天的百分比）
+          statistic.value.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease / nowRate));
+          // todayViewIncreaseRate：今日预计增长率
+          // 今天增长率=（今天预计-昨天）/昨天
+          // 正值（负值）：显示一个向上（向下）的箭头
+          statistic.value.todayViewIncreaseRate = (statistic.value.todayViewIncrease - statisticResp[0].viewIncrease) / statisticResp[0].viewIncrease * 100;
+          // 真正显示的增长率（绝对值）
+          statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
+        }
+      });
+    };
+
+    onMounted(() => {
+      getStatistic();
+    });
+
+    return {
+      statistic
     }
   }
 });
