75b1c879879f8a45ba6428a9c27d492fad8a87d3
wxh <13546675295@163.com>
2022-06-12 11:09:54 +0800

    11-8 集成echarts5.3.2。增加示例

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -87,12 +87,21 @@
         </a-card>
       </a-col>
     </a-row>
+    <!-- 空行，使上下之间有个间距  -->
+    <br>
+    <a-row>
+      <a-col :span="24">
+        <div id="main" style="width: 100%;height:300px;"></div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent, ref, onMounted} from 'vue'
 import axios from 'axios';
+
+declare let echarts:any;
 
 export default defineComponent({
   name: 'the-welcome',
@@ -140,8 +149,41 @@ export default defineComponent({
       });
     };
 
+    const testEcharts = () => {
+      // 基于准备好的dom，初始化echarts实例
+      // 这里的echarts是全局的变量，是属于Vue外部的，需要使用declare声明一下
+      // 又因为echarts的示例使用的是原生的JS（当时使用的是var），现在得改成const
+      const  myChart = echarts.init(document.getElementById('main'));
+
+      // 指定图表的配置项和数据
+      const option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    }
+
     onMounted(() => {
       getStatistic();
+      testEcharts();
     });
 
     return {
